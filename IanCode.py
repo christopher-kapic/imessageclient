7f9619a4f46f40fc1f2ff6d8fcd42c51a20df502
Ian Cramer <iancramer@Ians-MacBook-Pro.local>
2020-08-26 14:03:46 -0500

    Minor bug fixes. Added timing proceses, terminal printing, and comments for debugging.

--- IanCode.py
+++ IanCode.py
@@ -13,7 +13,7 @@ import os
 import sys
 import json
 
-start_time = datetime.now()
+# start_time = datetime.now() # For Timing Purposes
 
 ######################################################################
 ################################ Constants ###########################
@@ -36,9 +36,10 @@ NUM_MESSAGES = 50
 ######################################################################
 ############################## Objects ###############################
 class Conversation(object):
-	def __init__(self, contact, handleId):
+	def __init__(self, contact, handleId, phoneNumber):
 		self.contact = contact
 		self.handleId = handleId
+		self.phoneNumber = phoneNumber
 		self.messages = []
 
 	def __repr__(self):
@@ -57,7 +58,7 @@ class Conversation(object):
 
 
 	def addMessages(self, messages):
-		messages.reverse()
+		# messages.reverse()
 		self.messages = messages
 
 	def json(self):
@@ -95,6 +96,9 @@ def getName(id):
 
 	return buddy
 
+def timeToFloat(time):
+	return float(str(time).split(':')[-1])
+
 
 
 ######################################################################
@@ -120,37 +124,44 @@ data.reverse()
 
 
 
-
+# Get Handle IDs from the data extracted from the "Messages" table above. The Handle ID of a person in your "Messages" table is the number person you sent a text to. i.e. A person with Handle ID 78 is the 78th person you sent a text to. Your phone uses this to recognize which contact you are sending a message to or receiving a message from.
 handleIds = []
 for m in data:
 	if len(handleIds) == NUM_CONVERSATIONS:
 		break
-	elif m[0]:
+	elif m[0] in handleIds or m[0] == 0:
+		pass
+	else:
 		handleIds.append(m[0])
 
 
 
-
-rowIds = []
+# Get Phone Numbers. The ID of a person in the "Handle" table is the phone number of the person you are sending a text to or receiving a text from. The Row ID of a person in the "Handle" table is the same as the Handle ID of a message in the "Messages" table
+phoneNumbers = []
 for hId in handleIds:
 	c.execute(f"SELECT id FROM handle WHERE ROWID={hId};")
-	rowIds.append(c.fetchall()[0][0])
+	phoneNumbers.append(c.fetchall()[0][0])
 
 
+
+# This block of code runs an applescript process that recovers the contact names of the phone numbers extracted from the "Handle" table, then uses those contact names to create Conversation objects that store the names of contacts and the recent messages exchanged with them.
+# TIME CONSUMER
+# convoStart_time = datetime.now() # For Timing Purposes
 
 conversations = []
 for i in range(len(handleIds)):
-	convo = Conversation(getName(rowIds[i]), handleIds[i])
+	convo = Conversation(getName(phoneNumbers[i]), handleIds[i], phoneNumbers[i])
 	conversations.append(convo)
 
+# convo_time = datetime.now()-convoStart_time # For Timing Purposes
 
 
 
+# Collecting Messages Again. This time we are collecting the text of each messages sent and received as well as who sent the text message ("is_from_me" field) to store them in the Conversation objects.
 for convo in conversations:
 	c.execute(f"SELECT is_from_me, text FROM message WHERE handle_id={convo.handleId} and datetime(message.date/1000000000 + strftime('%s', '2001-01-01') ,'unixepoch','localtime') > '{lastMonth}';")
 	data = c.fetchall()
-	data.reverse()
-	convo.addMessages(data[0:NUM_MESSAGES])
+	convo.addMessages(data[-NUM_MESSAGES:])
 
 
 
@@ -166,8 +177,11 @@ for convo in conversations:
 
 ######################################################################
 
+# tRunTime = datetime.now() - start_time # For Timing Purposes
+# print('Total Run Time:', tRunTime) # For Timing Purposes
+# print('Conversations Time:', convo_time) # For Timing Purposes
+# print(timeToFloat(tRunTime)-timeToFloat(convo_time)) # For Timing Purposes
 
-print(datetime.now() - start_time)
 
 
 
