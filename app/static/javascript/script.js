8b98d763afc882a1421ddaac7beca6eab9968456
Christopher Kapic <christopherkapic@christophersmbp.lan>
2020-08-27 16:03:36 -0500

    aesthetic changes, working on POST

--- app/static/javascript/script.js
+++ app/static/javascript/script.js
@@ -1,4 +1,21 @@
+let global_data;
+let global_vars;
 
+function loadConversation(cont) {
+    document.getElementById("active-con-header").innerHTML = cont;
+    for (i = 0; i < global_data.length; i++) {
+        if (global_data[i]["contact"] == cont) {
+            for (const message of global_data[i]["messages"]) {
+                if (message[0] == 0) { // other person
+                    document.getElementById("message-box").innerHTML += '<p>' + message[1] + '</p>';
+                } else { // user
+                    document.getElementById("message-box").innerHTML += '<p>' + message[1] + '</p>';
+                }
+            }
+        }
+    }
+    document.getElementById("pending-message-text").addEventListener('submit', sendText);
+}
 
 function firstLoadMessages() {
     fetch("/api/init")
@@ -13,10 +30,24 @@ function firstLoadMessages() {
         })
         .then(data => {
             console.log(data);
+            global_data = data;
             for (i = 0; i < data.length; i++) {
-                document.getElementById("conversations").innerHTML += '<div class="conversation"><h1 class="conversation-contact">' + data[i]["contact"] + '<h1><p class="conversation-latest">' + data[i]["messages"][0][1] + '<p><div>'
+                document.getElementById("conversations").innerHTML += '<div class="conversation"><h1 class="conversation-contact">' + data[i]["contact"] + '<h1><p class="conversation-latest">' + data[i]["messages"][0][1] + '<p><div>';
             }
+            loadConversation(data[0]["contact"]);
         })
+}
+
+function sendText() {
+    let mymes = document.getElementById("pending-message-text").value;
+    fetch("/api/message", {
+        method: 'POST',
+        body: {
+            "recipient": "Ian Cramer",
+            "text": mymes
+        }
+    })
+
 }
 
 function showAlert() {
